332bf61c1ebd2cf9fbf61fce430f873c1d16fc1f
arekasek <110391257+arekasek@users.noreply.github.com>
2023-07-12 21:22:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,103 +1,126 @@
-let wynik=0;
-let liczba1=document.querySelector(".liczba1");
+let wynik = 0;
+let liczba1 = document.querySelector(".liczba1");
 let liczba2 = document.querySelector('.liczba2 span');
-let liczby=document.querySelectorAll('.liczba');
-let operatory=document.querySelectorAll('.operator');
-let rowna=document.getElementById('rowna');
-let wlacz=document.getElementById('wlacz');
-let dod=document.getElementById('dod');
-let pot=document.getElementById('pot');
-let pier=document.getElementById('pier');
-let op=document.getElementById('op');
-let rezultat=document.getElementById('rezultat');
+let liczby = document.querySelectorAll('.liczba');
+let operatory = document.querySelectorAll('.operator');
+let rowna = document.getElementById('rowna');
+let wlacz = document.getElementById('wlacz');
+let dod = document.getElementById('dod');
+let pot = document.getElementById('pot');
+let pier = document.getElementById('pier');
+let op = document.getElementById('op');
+let rezultat = document.getElementById('rezultat');
+let kropkaUzyta = false;
 
 function wyswietlN() {
-    if(liczba1.innerHTML.startsWith('0')){
-        liczba1.innerHTML='';
-    }
-    liczba1.innerHTML+=this.textContent;
-}
-function potegowanie(){
-        if(liczba2.innerHTML===''){
-        wynik=Math.pow(parseFloat(liczba1.innerHTML),2);
-        liczba1.innerHTML='';
-        liczba1.innerHTML+=wynik;
-        }
-}
-function pierwiastkowanie(){
-        if(liczba2.innerHTML===''){
-        wynik=Math.sqrt(parseFloat(liczba1.innerHTML));
-        liczba1.innerHTML='';
-        liczba1.innerHTML+=wynik;
-        }
-}
-var o=0;
-function dzialanie(){
-        if(op.innerHTML===''){
-        liczba2.innerHTML=liczba1.innerHTML;
-        op.innerHTML=this.textContent;
-        liczba1.innerHTML='';
-        }
-        else if(op.innerHTML=this.textContent){
-        op.innerHTML=this.textContent;
-        liczba1.innerHTML='';
-        }
+  if (liczba1.innerHTML.startsWith('0')) {
+    liczba1.innerHTML = '';
+  }
+  liczba1.innerHTML += this.textContent;
 }
 
-function wyniki(){
-    function czyszczenie(){
-        liczba1.innerHTML='';
-        rezultat.innerHTML='';
-        op.innerHTML='';
-    }
-    function wyswietlW(){
-        liczba1.innerHTML+=wynik;
-    }
-    if(op.innerHTML==='x'){
-        wynik=parseFloat(liczba2.innerHTML)*parseFloat(liczba1.innerHTML);
-        czyszczenie();
-        wyswietlW();
-    }
+function potegowanie() {
+  if (liczba2.innerHTML === '') {
+    wynik = Math.pow(parseFloat(liczba1.innerHTML), 2);
+    liczba1.innerHTML = '';
+    liczba1.innerHTML += wynik;
+  }
+}
 
-    else if(op.innerHTML==='÷'){
-        wynik=parseFloat(liczba2.innerHTML)/parseFloat(liczba1.innerHTML);
-        czyszczenie();
-        wyswietlW();
-    }
+function pierwiastkowanie() {
+  if (liczba2.innerHTML === '') {
+    wynik = Math.sqrt(parseFloat(liczba1.innerHTML));
+    liczba1.innerHTML = '';
+    liczba1.innerHTML += wynik;
+  }
+}
 
-    else if(op.innerHTML==='%'){
-        wynik=parseFloat((liczba2.innerHTML)/100)*parseFloat(liczba1.innerHTML);
-        czyszczenie();
-        wyswietlW();
-    }
-    else if(op.innerHTML==='+'){
-        wynik=parseFloat(liczba2.innerHTML)+parseFloat(liczba1.innerHTML);
-        czyszczenie();
-        wyswietlW();
-    }
-    else if(op.innerHTML==='-'){
-        wynik=parseFloat(liczba2.innerHTML)-parseFloat(liczba1.innerHTML);
-        czyszczenie();
-        wyswietlW();
-    }
-    }
-    
+var o = 0;
 
-pot.addEventListener("click",potegowanie);
-pier.addEventListener("click",pierwiastkowanie);
-rowna.addEventListener("click",wyniki);
+function dzialanie() {
+  if (op.innerHTML === '') {
+    liczba2.innerHTML = liczba1.innerHTML;
+    op.innerHTML = this.textContent;
+    liczba1.innerHTML = '';
+    kropkaUzyta = false;
+  } else if (op.innerHTML = this.textContent) {
+    op.innerHTML = this.textContent;
+    liczba1.innerHTML = '';
+    kropkaUzyta = false;
+  }
+}
 
-wlacz.addEventListener("click",()=>{
-    rezultat.innerHTML='';
-    op.innerHTML='';
-    liczba1.innerHTML='0';
-})
+function wyniki() {
+  function czyszczenie() {
+    liczba1.innerHTML = '';
+    rezultat.innerHTML = '';
+    op.innerHTML = '';
+    kropkaUzyta = false;
+  }
 
-liczby.forEach((button)=> {
-    button.addEventListener('click',wyswietlN);
-});
-operatory.forEach((button)=> {
-    button.addEventListener('click',dzialanie);
+  function wyswietlW() {
+    liczba1.innerHTML += wynik;
+  }
+
+  if (op.innerHTML === 'x') {
+    wynik = parseFloat(liczba2.innerHTML) * parseFloat(liczba1.innerHTML);
+    czyszczenie();
+    wyswietlW();
+  } else if (op.innerHTML === '÷') {
+    wynik = parseFloat(liczba2.innerHTML) / parseFloat(liczba1.innerHTML);
+    czyszczenie();
+    wyswietlW();
+  } else if (op.innerHTML === '%') {
+    wynik = parseFloat(liczba2.innerHTML) / 100 * parseFloat(liczba1.innerHTML);
+    czyszczenie();
+    wyswietlW();
+  } else if (op.innerHTML === '+') {
+    wynik = parseFloat(liczba2.innerHTML) + parseFloat(liczba1.innerHTML);
+    czyszczenie();
+    wyswietlW();
+  } else if (op.innerHTML === '-') {
+    wynik = parseFloat(liczba2.innerHTML) - parseFloat(liczba1.innerHTML);
+    czyszczenie();
+    wyswietlW();
+  }
+}
+
+pot.addEventListener("click", potegowanie);
+pier.addEventListener("click", pierwiastkowanie);
+rowna.addEventListener("click", wyniki);
+
+wlacz.addEventListener("click", () => {
+  rezultat.innerHTML = '';
+  op.innerHTML = '';
+  liczba1.innerHTML = '0';
+  kropkaUzyta = false;
 });
 
+liczby.forEach((button) => {
+  button.addEventListener('click', function () {
+    if (this.textContent === '.') {
+      if (!kropkaUzyta) {
+        wyswietlN.call(this);
+        kropkaUzyta = true;
+      }
+    } else {
+      wyswietlN.call(this);
+    }
+  });
+});
 
+operatory.forEach((button) => {
+  button.addEventListener('click', dzialanie);
+});
+
+function wyswietlN() {
+    if (this.textContent === '.') {
+      if (!liczba1.innerHTML.includes('.')) {
+        liczba1.innerHTML += this.textContent;
+      }
+    } else if (liczba1.innerHTML === '0') {
+      liczba1.innerHTML = this.textContent;
+    } else {
+      liczba1.innerHTML += this.textContent;
+    }
+  }
